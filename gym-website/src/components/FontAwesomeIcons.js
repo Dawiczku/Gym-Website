c46822e1fcfa6afb2b98e2ddf7164b6d043c3d94
Dawiczku <skarupski.dawid@gmail.com>
2023-03-28 08:50:29 +0200

    Install font awesome icons

---
+++ gym-website/src/components/FontAwesomeIcons.js
@@ -0,0 +1,9 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faFacebook, faInstagram, faYoutube, faGithub);
