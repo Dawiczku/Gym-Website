ea14d59ab1d9c9af0caad765936b3f5779880f5e
Dawiczku <skarupski.dawid@gmail.com>
2023-03-30 09:59:10 +0200

    Create and style the Login section

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -2,16 +2,18 @@ import Main from "./components/Main";
 import About from "./components/About";
 import Plans from "./components/Plans";
 import Contact from "./components/Contact";
+import Login from "./components/Login";
 import "./components/FontAwesomeIcons";
 import "./styles/styles.css";
 
 function App() {
   return (
     <>
-      <Main />
-      <About />
-      <Plans />
-      <Contact />
+      {/* <Main /> */}
+      {/* <About /> */}
+      {/* <Plans /> */}
+      {/* <Contact /> */}
+      <Login />
     </>
   );
 }
---
+++ gym-website/src/components/Login.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <section className="section--login text-clr--primary bg-clr--form-section">
+        <form className="login__form clr-border--grey bg-clr--dark">
+          <h1 className="font--bold accent--clr">Login !</h1>
+
+          <input
+            className="nickname__input text-clr--primary"
+            type="text"
+            name="nickname"
+            placeholder="Nickname"
+            maxLength={15}
+            required
+          ></input>
+          <input
+            className="password__input text-clr--primary"
+            type="password"
+            name="password"
+            placeholder="Password"
+            maxLength={15}
+            required
+          ></input>
+          <p className="forgot-password__link font--bold">Forgot Password ?</p>
+          <input
+            className="login__button button--style-second text-clr--primary"
+            type="submit"
+            value="Sign In"
+          ></input>
+          <p className="signup__link font--bold">Not a member ? Sign up!</p>
+        </form>
+      </section>
+    </>
+  );
+}
